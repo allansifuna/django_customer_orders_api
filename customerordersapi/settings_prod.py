edf29fd5e8a6acae438bcb4e8b93d64b7c9247ac
Allan Sifuna <allansifuna324@gmail.com>
2023-09-16 15:06:29 +0300

    Decoupled settings environments

---
+++ customerordersapi/settings_prod.py
@@ -0,0 +1,17 @@
+import environ
+from .settings import *
+
+env = environ.Env()
+
+
+DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+    }
+}
--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'customerordersapi.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
