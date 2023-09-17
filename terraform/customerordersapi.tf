220a7239db19dd88e3b7045e276224d79f0cf7e0
Allan Sifuna <allansifuna324@gmail.com>
2023-09-17 14:26:36 +0300

    Deploy to linode kubernetes Engine

---
+++ terraform/customerordersapi.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "1.29.4"
+    }
+  }
+}
+
+provider "linode" {
+  token = var.token
+}
+
+resource "linode_lke_cluster" "customerordersapi" {
+    k8s_version = var.k8s_version
+    label = var.label
+    region = var.region
+    tags = var.tags
+
+    dynamic "pool" {
+        for_each = var.pools
+        content {
+            type  = pool.value["type"]
+            count = pool.value["count"]
+        }
+    }
+}
+
+output "kubeconfig" {
+  value = linode_lke_cluster.customerordersapi.kubeconfig
+  sensitive = true
+}
+
+output "api_endpoints" {
+  value = linode_lke_cluster.customerordersapi.api_endpoints
+}
+
+output "status" {
+  value = linode_lke_cluster.customerordersapi.status
+}
+
+output "id" {
+  value = linode_lke_cluster.customerordersapi.id
+}
+
+output "pool" {
+  value = linode_lke_cluster.customerordersapi.pool
+}
+
